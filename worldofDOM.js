7da34e4eb05cb8f47b1f473fb2c4f4829c0f29f3
Maganga Mwambonu <codebymaganga@gmail.com>
2024-09-04 21:01:32 +0300

    DOM selectors and Attributes

---
+++ worldofDOM.js
@@ -0,0 +1,43 @@
+//A node simply consists of data and the adress  of where this data can be accessed
+
+/**
+ * ways of accessing elements inside our DOM tree
+ * 1.QuerySelector() ---> it enables u to access any element in ur DOM
+ * the catch is it only return the first element
+ * document.querySelector('h2') 
+ * to write our query selector simply pass in the name of the element inside the parentheses with a quotation
+ */
+
+/**
+ * 2.getElementbyID()
+ * Gets an element by ID
+ * return the first element found in the case we have two or more elements withthe same ID
+ */
+
+/**
+ * 3.querySelectorAll()
+ * return all elements found
+ */
+
+/**
+ * 4.getElementsByClassName()
+ * returns all elements with the class name
+ * 
+ */
+
+/**
+ * 5.getElementsByTagName()
+ * this one works the same as query selectory only difference is this returns all the elements
+ * return an array of all elements
+ */
+
+//ATTRIBUTES
+/**
+ * to get an attribute
+ * 1.get the element using queryselector or get element by ID
+ * 2.element.getAttribute(name of Attribute)
+ */
+
+
+
+
