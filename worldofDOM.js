b34f5d20213add6545542711d3080c66bcfff5e1
Maganga Mwambonu <codebymaganga@gmail.com>
2024-09-05 21:13:29 +0300

    styling,dom manipulation

--- worldofDOM.js
+++ worldofDOM.js
@@ -35,9 +35,38 @@
 /**
  * to get an attribute
  * 1.get the element using queryselector or get element by ID
- * 2.element.getAttribute(name of Attribute)
+ * 2.element.getAttribute(name of Attribute eg class)
  */
 
+//setAttribute() -> 2 parameters, 1st parameter -> name of the attribute,2nd parameter->'textFive'
+
+
+//styling elements using selectors
+/**
+ * 1.get the element first using selectors
+ * 2.accessing the styles properties using either dotNotation or bracketNotation
+ * eg element.style.NameOfStyle = 'passInYourValue'
+ */
+
+
+//creating elements
+/**
+ * 1.first you create an element using document -> .createElement
+ * 2.to add this to the dom we use .AppendChild()
+ */
+
+/**
+ * 1.make your parent div using create element,store inside your parent variable
+ * 2.make other divs using create element and store them inside your child variable
+ * 3.to add the small divs to the big divs use appendChild
+ */
+
+//appendChild adds children to parents
+
+
+//while using innerHTML it ignores the markup tags
+
+//while using textContent and InnerText it includes the markup tags
 
 
 
